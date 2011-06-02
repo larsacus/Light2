e983d79822fbc8b4caa2e18b7532b100865cc4a6
Lars Anderson <ishka912@gmail.com>
2011-06-02 03:21:19 +0000

    + Initial Import

---
+++ Light²/Light_ViewController.h
@@ -0,0 +1,15 @@
+//
+//  Light_ViewController.h
+//  Light²
+//
+//  Created by Lars Anderson on 6/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Light_ViewController : UIViewController {
+    
+}
+
+@end
