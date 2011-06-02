bd13d3339019e3981c7c983df856a035ad54578a
Lars Anderson <ishka912@gmail.com>
2011-06-02 03:21:58 +0000

    + Initial Import

---
+++ Light²Tests/Light_Tests.h
@@ -0,0 +1,17 @@
+//
+//  Light_Tests.h
+//  Light²Tests
+//
+//  Created by Lars Anderson on 6/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface Light_Tests : SenTestCase {
+@private
+    
+}
+
+@end
